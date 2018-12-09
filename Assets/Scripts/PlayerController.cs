bea73237a4aa290bdae999bf2580e4ad5f4b1dc3
DELL\nslez <nslezak94@gmail.com>
2018-12-09 01:13:17 +0100

    Force pulling player towards planet.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+
+	public Vector2 gravityFactor;
+	private Rigidbody2D rb;
+	private GameObject planet;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody2D>();
+		planet = GameObject.FindGameObjectWithTag("Planet");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Vector2 acceleration = planet.transform.position - this.transform.position;
+		rb.AddForce(acceleration * rb.mass * gravityFactor); 
+	}
+
+	private void FixedUpdate()
+	{
+		throw new System.NotImplementedException();
 	}
 }
