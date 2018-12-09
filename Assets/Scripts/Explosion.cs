aa0040c04f4bd929f3e13383d4f7ea7c4cb4d3e8
kbatko <kbatko11@gmail.com>
2018-12-09 04:03:05 +0100

    explosion

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+         Destroy(gameObject);
+    }
+
+    public Vector2 velo;
+    private Rigidbody2D rb;
+    public float length;
+
+    // Use this for initialization
+    void Start () {
+        rb = this.GetComponent<Rigidbody2D>();
+        velo = rb.velocity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.GetComponent<Rigidbody2D>().velocity.magnitude < 0)
+        {
+            Destroy(gameObject);
+        }
+        length = this.GetComponent<Rigidbody2D>().velocity.magnitude;
+    }
+}
---
+++ Assets/Scripts/PlanetCollision.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetCollision : MonoBehaviour {
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+    }
+}
